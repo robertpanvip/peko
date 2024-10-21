814ea9bfc16dfb9922bce35c286e108cff901198
robertpanvip <robertpanvip@163.com>
2024-10-21 09:42:43 +0800

    Update query.ts

--- src/query.ts
+++ src/query.ts
@@ -1,8 +1,16 @@
-//获取url查询参数
-import {AnyObject} from "./interface";
+import { AnyObject } from "./interface";
 
-export function getQuery<T extends AnyObject<string> = AnyObject<string>>(href: string = window.location.href) {
+export function getQuery<T extends AnyObject<string | string[]> = AnyObject<string | string[]>>(href: string = window.location.href) {
   const searchParams = new URL(href.replace("#", "")).searchParams;
   const entries = new URLSearchParams(searchParams).entries();
-  return Object.fromEntries(entries) as T;
+  
+  const queryObject = {} as AnyObject<string | string[]>;
+  
+  for (const [key, value] of entries) {
+    // 如果值包含逗号，则将其解析为数组，否则保留为字符串
+    queryObject[key] = value.includes(",") ? value.split(",") : value;
+  }
+
+  return queryObject as T;
 }
+
