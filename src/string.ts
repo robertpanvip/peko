4ea7168b3b0ea75e3dbffe400dc5c9684d44ce1a
robertpanvip <robertpanvip@163.com>
2024-10-21 10:35:29 +0800

    Update string.ts

--- src/string.ts
+++ src/string.ts
@@ -21,3 +21,10 @@ export function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+export function shortUUID() {
+    const timestamp = Date.now().toString(36); // 当前时间戳转为 base36
+    const random = Math.random().toString(36).substring(2, 8); // 生成随机字符串
+    return timestamp + random; // 合并两者
+}
+
+
